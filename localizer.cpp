424cdcc96de2a7ca0a4fae120dac396d503d9d79
Filip Parag <filiparag.com@gmail.com>
2018-12-25 14:03:44 +0100

    Generate markers

--- localizer.cpp
+++ localizer.cpp
@@ -1,22 +1,83 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/aruco.hpp>
 
-using namespace cv;
+void createMarkers(unsigned number) {
 
-int main()
-{
-    VideoCapture cap(-1);     // get 'any' cam
-    while( cap.isOpened() )   // check if we succeeded
-    {
-        Mat frame;
-        if ( ! cap.read(frame) )
-            break;
+    std::filesystem::remove_all("markers/images");
+    std::filesystem::create_directories("markers/images");
 
-        threshold(frame, frame, 50, 255, 1);
-        imshow("Webcam",frame);
-        int k = waitKey(33);
-        if ( k==27 )
-            break;
+    cv::Mat markerImage;
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_100);
+    char fileName[256];
+
+    for (auto i = 0; i < number; ++i) {
+        std::snprintf(fileName, 256, "markers/images/%i.png", i);
+        dictionary.cv::aruco::Dictionary::drawMarker(i, 512, markerImage, 1);
+        cv::imwrite(fileName, markerImage);
     }
+
+}
+
+int main(int argc, char** argv)
+{
+    // VideoCapture cap(-1);     // get 'any' cam
+    // while( cap.isOpened() )   // check if we succeeded
+    // {
+    //     Mat frame;
+    //     if ( ! cap.read(frame) )
+    //         break;
+
+    //     Mat frame1, frame2, frame3, frame4;
+
+    //     cvtColor(frame, frame1, cv::COLOR_BGR2HSV);
+
+    //     Mat planes[3];
+
+    //     split(frame1, planes);
+
+    //     inRange(planes[0], 20, 40, frame2);
+
+    //     inRange(planes[2], 150, 255, frame3);
+
+    //     Mat element = getStructuringElement( 0,
+    //                                    Size( 20, 20 ),
+    //                                    Point( 1, 1 ) );
+
+    //     erode(frame2 & frame3, frame4, element);
+
+    //     std::vector<std::vector<cv::Point> > contours;
+    //     cv::Mat contourOutput = frame4.clone();
+    //     cv::findContours( contourOutput, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE );
+
+    //     //Draw the contours
+    //     cv::Mat contourImage(frame4.size(), CV_8UC3, cv::Scalar(0,0,0));
+    //     cv::Scalar colors[3];
+    //     colors[0] = cv::Scalar(255, 0, 0);
+    //     colors[1] = cv::Scalar(0, 255, 0);
+    //     colors[2] = cv::Scalar(0, 0, 255);
+    //     for (size_t idx = 0; idx < contours.size(); idx++) {
+    //         cv::drawContours(contourImage, contours, idx, colors[idx % 3]);
+    //     }
+
+
+    //     imshow("Webcam", frame);
+    //     imshow("Filter", contourImage);
+    //     int k = waitKey(33);
+    //     if ( k==27 )
+    //         break;
+    // }
+
+    if (argc > 1) {
+        if (argv[1] == std::string("generate")) {
+            createMarkers(atoi(argv[2]));
+        }
+    }
+    
     return 0;
 }
