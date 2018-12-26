5e620d936a1ff60500521be58a5f603276f09c30
Filip Parag <filiparag.com@gmail.com>
2018-12-26 12:14:54 +0100

    Border size

--- markers.cpp
+++ markers.cpp
@@ -5,13 +5,14 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
 
-#define FOLDER_MARKERS std::string("markers/")
-#define FOLDER_MARKER_IMAGES std::string("images/")
-
-#define MARKER_IMAGE_SIZE 512
-#define MARKER_IMAGE_TYPE std::string(".png")
-
 namespace markers {
+
+    #define FOLDER_MARKERS std::string("markers/")
+    #define FOLDER_MARKER_IMAGES std::string("images/")
+
+    #define MARKER_IMAGE_SIZE 512
+    #define MARKER_BORDER_SIZE 1
+    #define MARKER_IMAGE_TYPE std::string(".png")
 
     void generateImages(unsigned number) {
 
@@ -24,7 +25,7 @@ namespace markers {
         for (auto i = 0; i < number; ++i) {
             std::stringstream fileName;
             fileName << FOLDER_MARKERS << FOLDER_MARKER_IMAGES << i << MARKER_IMAGE_TYPE;
-            dictionary.cv::aruco::Dictionary::drawMarker(i, MARKER_IMAGE_SIZE, markerImage, 1);
+            dictionary.cv::aruco::Dictionary::drawMarker(i, MARKER_IMAGE_SIZE, markerImage, MARKER_BORDER_SIZE);
             cv::imwrite(fileName.str(), markerImage);
         }
 
